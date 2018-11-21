e67e6647e5b01a26559242b66d0b7f2ca4fb90cc
andywynkop <andywynkop@gmail.com>
2018-11-21 14:48:23 -0800

    Complete phase II of RailsLite

--- skeleton/bin/p01_basic_server.rb
+++ skeleton/bin/p01_basic_server.rb
@@ -4,7 +4,7 @@ app = Proc.new do |env|
   req = Rack::Request.new(env)
   res = Rack::Response.new
   res['Content-Type'] = 'text/html'
-  res.write("Hello world!")
+  res.write(req.path_info)
   res.finish
 end
 
--- skeleton/lib/controller_base.rb
+++ skeleton/lib/controller_base.rb
@@ -8,20 +8,37 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    unless already_built_response?
+      @res.header['location'] = url
+      @res.status = 302
+      @already_built_response = true
+    else
+      raise 'Cannot redirect twice!!!!'
+    end
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    unless already_built_response?
+      @res.write(content)
+      @res["Content-Type"] = content_type
+      @already_built_response = true
+    else
+      raise 'Cannot call render twice!!!!!'
+    end
   end
 
   # use ERB and binding to evaluate templates
@@ -37,4 +54,3 @@ class ControllerBase
   def invoke_action(name)
   end
 end
-
